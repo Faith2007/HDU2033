98e876fea7adb00457d7608ae44960f2d4d21b29
Faith2007 <2484563264@qq.com>
2026-09-21 23:28:05 +0800

    second save

--- HDU2033.c
+++ HDU2033.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i;
+    int h1,m1,s1;
+    int h2,m2,s2;
+
     scanf("%d",&n);
-    int h[100][3],m[100][3],s[100][3];
-    for(i=0;i<n;i++)
+
+    for (i=0;i<n;i++)
     {
-        for(j=0;j<=2;j++)
-        {
-            h[i][j]=0;
-            m[i][j]=0;
-            s[i][j]=0;
-        }
+        scanf("%d%d%d%d%d%d",&h1,&m1,&s1,&h2,&m2,&s2);
+
+        s1=s1+s2;
+        m1=m1+m2+s1/60;
+        h1=h1+h2+m1/60;
+
+        printf("%d %d %d\n",h1,m1%60,s1%60);
     }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<2;j++)
-        {
-            scanf("%d%d%d",&h[i][j],&m[i][j],&s[i][j]);
-        }
-        s[i][2]=(s[i][0]+s[i][1])%60;
-        m[i][2]=(m[i][0]+m[i][1]+(s[i][0]+s[i][1])/60)%60;
-        h[i][2]=h[i][0]+h[i][1]+(m[i][0]+m[i][1]+(s[i][0]+s[i][1])/60)/60;
-    }
-    for(i=0;i<n;i++)
-    {
-        printf("%d %d %d\n",h[i][2],m[i][2],s[i][2]);
-    }
+
     return 0;
 }
